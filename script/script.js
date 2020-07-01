2bf8d5f81df8a835ae0093fa3c58e3b28fc71cbd
Alex Moiseev <coding-awm@mail.ru>
2020-07-01 22:38:36 +0600

    сделал нули и остановку таймера

--- script/script.js
+++ script/script.js
@@ -2,36 +2,54 @@ window.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
   //'это таймер
-  function countTimer(deadLine) {
+  function countTimer(deadLIne) {
     let timerHours = document.getElementById('timer-hours');
     let timerMinutes = document.getElementById('timer-minutes');
     let timerSeconds = document.getElementById('timer-seconds');
 
     function getTimeRemaining() {
-      let dateStop = new Date(deadLine).getTime();
+      let dateStop = new Date(deadLIne).getTime();
       let dateNow = new Date().getTime();
       let timeRemaining = (dateStop - dateNow) / 1000;
       let seconds = Math.floor(timeRemaining % 60);
       let minutes = Math.floor((timeRemaining / 60) % 60);
       let hours = Math.floor(timeRemaining / 60 / 60);
-      return {
-        timeRemaining,
-        seconds,
-        minutes,
-        hours,
-      };
+      if (timeRemaining < 0) {
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+        clearInterval(idInterval)
+      } else {
+        return {
+          timeRemaining,
+          seconds,
+          minutes,
+          hours,
+        };
+
+      }
     }
-    
+
     //let days = Math.floor(timeRemaining / 60 / 60 / 24);// если нужны дни
-    setInterval(function updateClock() {
+    // let idInterval
+    let idInterval = setInterval(function updateClock() {
       let timer = getTimeRemaining();
-      timerHours.textContent = timer.hours;
-      timerMinutes.textContent = timer.minutes;
-      timerSeconds.textContent = timer.seconds;
+      timerHours.textContent = setZero(timer.hours);
+      timerMinutes.textContent = setZero(timer.minutes);
+      timerSeconds.textContent = setZero(timer.seconds);
     }, 1000);
     
+    
+
+    function setZero(item) {
+      if (item < 10) {
+        return '0' + item;
+      } else {
+        return item;
+      }
+    }
   }
 
   countTimer('5 july 2020');
-  // setInterval()
+  
 });

4ba4dc1e3ca558c1d587edb16089d4e57bf93154
Alex Moiseev <coding-awm@mail.ru>
2020-07-01 21:46:48 +0600

    сделал через сетИнтервал

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoader', function () {
+window.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
   //'это таймер
@@ -6,12 +6,32 @@ window.addEventListener('DOMContentLoader', function () {
     let timerHours = document.getElementById('timer-hours');
     let timerMinutes = document.getElementById('timer-minutes');
     let timerSeconds = document.getElementById('timer-seconds');
-    let dateStop = new Date(deadLine).getTime();
-    let dateNow = new Date().getTime();
 
-    console.log(dateStop);
-    console.log(dateNow);
+    function getTimeRemaining() {
+      let dateStop = new Date(deadLine).getTime();
+      let dateNow = new Date().getTime();
+      let timeRemaining = (dateStop - dateNow) / 1000;
+      let seconds = Math.floor(timeRemaining % 60);
+      let minutes = Math.floor((timeRemaining / 60) % 60);
+      let hours = Math.floor(timeRemaining / 60 / 60);
+      return {
+        timeRemaining,
+        seconds,
+        minutes,
+        hours,
+      };
+    }
+    
+    //let days = Math.floor(timeRemaining / 60 / 60 / 24);// если нужны дни
+    setInterval(function updateClock() {
+      let timer = getTimeRemaining();
+      timerHours.textContent = timer.hours;
+      timerMinutes.textContent = timer.minutes;
+      timerSeconds.textContent = timer.seconds;
+    }, 1000);
+    
   }
 
   countTimer('5 july 2020');
+  // setInterval()
 });

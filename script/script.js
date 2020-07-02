a5412dee2d61ff0a403534b4384444fc891b2ea6
Alex Moiseev <coding-awm@mail.ru>
2020-07-02 09:51:51 +0600

    оформил 18 доп

---
+++ script/script.js
@@ -0,0 +1,9 @@
+'use srtict';
+
+let hello = document.querySelector('.hello');
+let present = document.querySelector('.present');
+let nowTime = document.querySelector('.now-time');
+let xMas = document.querySelector('.x-mas');
+
+let date = new Date();
+let neeYear = new Date(2021, 0, 1);

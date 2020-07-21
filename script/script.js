5ca80ceab7f3ce366b07331afc2d649de763bfd1
Alex Moiseev <coding-awm@mail.ru>
2020-07-21 15:08:46 +0600

    вернул маску

--- script/script.js
+++ script/script.js
@@ -35,7 +35,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 			}
 		}
-		
+
 		function setZero(item) {
 			if (item < 10) {
 				return '0' + item;
@@ -348,36 +348,51 @@ window.addEventListener('DOMContentLoaded', () => {
 		const statusMessage = document.createElement('div');
 
 		const sendEachForm = ourForm => {
-
 			ourForm.addEventListener('input', event => {
 				const target = event.target;
-				const noShowNumber = function() {
-					this.value = this.value.replace(/[\da-zA-Z/*@#$]/g, '');
-				};
-				const showNumber = function() {
-					this.value = this.value.replace(/\D/g, '');
-				};
-				const noPhone = function() {
-					if (this.value.length > 0 && this.value.length !== 11) {
-						this.style.backgroundColor = '#FF5300';
-					} else {
-						this.style.backgroundColor = '';
-					}
+				const noShowNumber = function () {
+					this.value = this.value.replace(/[\da-zA-Z]/g, '');
 				};
 
+				function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+					const elems = document.querySelectorAll(selector);
+
+					function mask(event) {
+						const keyCode = event.keyCode;
+						const template = masked,
+							def = template.replace(/\D/g, ""),
+							val = this.value.replace(/\D/g, "");
+						// console.log(template);
+						let i = 0,
+							newValue = template.replace(/[_\d]/g, a => (i < val.length ? val.charAt(i++) || def.charAt(i) : a));
+						i = newValue.indexOf("_");
+						if (i != -1) {
+							newValue = newValue.slice(0, i);
+						}
+						let reg = template.substr(0, this.value.length).replace(/_+/g,
+							a => "\\d{1," + a.length + "}").replace(/[+()]/g, "\\$&");
+						reg = new RegExp("^" + reg + "$");
+						if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+							this.value = newValue;
+						}
+						if (event.type == "blur" && this.value.length < 5) {
+							this.value = "";
+						}
+					}
+
+					for (const elem of elems) {
+						elem.addEventListener("input", mask);
+						elem.addEventListener("focus", mask);
+						elem.addEventListener("blur", mask);
+					}
+				}
 
 				if (target.matches('.form-name') || target.matches('.mess')) {
 					target.addEventListener('input', noShowNumber);
 				}
-
 				if (target.matches('.form-phone')) {
-					target.addEventListener('input', showNumber);
+					target.addEventListener('input', maskPhone('.form-phone'));
 				}
-
-				if (target.matches('.form-phone')) {
-					target.addEventListener('blur', noPhone);
-				}
-
 			});
 
 			ourForm.addEventListener('submit', event => {
@@ -410,13 +425,11 @@ window.addEventListener('DOMContentLoaded', () => {
 					val.value = '';
 				});
 			});
-
 		};
 
 		sendEachForm(form[0]);
 		sendEachForm(form[1]);
 		sendEachForm(form[2]);
-
 
 		const postData = body =>
 
